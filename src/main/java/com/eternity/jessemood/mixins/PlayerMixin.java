2183256eede4beaffd23a2130ffb6aef88105205
ItsukaHiro <itsukahiro.personal@proton.me>
2025-03-01 23:53:44 +0700

    Update PlayerMixin.java

--- src/main/java/com/eternity/jessemood/mixins/PlayerMixin.java
+++ src/main/java/com/eternity/jessemood/mixins/PlayerMixin.java
@@ -15,7 +15,7 @@ public abstract class PlayerMixin {
         // System.out.println("Player was hurt!");
         // com.eternity.jessemood.client.guihandler.triggerDisplay(); // Set the display flag
         LocalPlayer player = (LocalPlayer) (Object) this;
-        if (player.isAlive() && player.getHealth() <= 2 && !player.isCreative()) {
+        if (player.isAlive() && player.getHealth() = 2 && !player.isCreative()) {
             System.out.println("Player is low on health!");
             com.eternity.jessemood.client.guihandler.triggerDisplay(); // Set the display flag
         }
